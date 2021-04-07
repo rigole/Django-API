78ba3f78e52df0bd115d66e810cf3620a9b76721
rigole <foplacide@gmail.com>
2021-04-07 06:46:55 +0100

    creating image field

--- mysite/movies/models.py
+++ mysite/movies/models.py
@@ -9,3 +9,4 @@ class Moviedata(models.Model):
     duration = models.FloatField()
     rating = models.FloatField()
     moviestype = models.CharField(max_length=200, default='action')
+    image = models.ImageField(upload_to='Images/', default="Images/None/Noimg.jpg")
